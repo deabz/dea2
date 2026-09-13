4b94c9a14703c1b7c47150e57f84f1b3b9b5632c
Arian <ibhuyn@gmail.com>
2026-09-13 19:42:21 +0100

    changed the interval from 10 seconds to 5 seconds

--- artifacts/api-server/src/discord/bot.ts
+++ artifacts/api-server/src/discord/bot.ts
@@ -1197,7 +1197,7 @@ export function startDiscordBot(): Client | null {
     }
     statusInterval = setInterval(() => {
       updateBotPresence(readyClient);
-    }, 10_000);
+    }, 5_000);
 
     logger.info(
       {
